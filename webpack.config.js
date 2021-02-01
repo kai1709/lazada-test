e030f9e029529820eab0ac31b737d27b3189896f
Son Trinh <sontrinh179@gmail.com>
2021-02-02 00:40:43 +0700

    [FE] Update webpack to load fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,36 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/App.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.scss$/i, use: ['style-loader', 'css-loader', 'sass-loader'] }
+      { test: /\.scss$/i, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        loader: "file-loader"
+      }
     ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, '..', 'static'),
+    hot: true,
+    publicPath: '/',
+    historyApiFallback: true,
   },
   mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
-    })
+    }),
+    new Dotenv()
   ]
 
 }
