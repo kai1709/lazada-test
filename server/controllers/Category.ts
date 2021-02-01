db6f0180d2d669e7eb5d5ecb30eb90a3f429fab1
Son Trinh <sontrinh179@gmail.com>
2021-02-02 00:41:33 +0700

    [BE] Add typescript for Category controllers

--- server/controllers/Category.ts
+++ server/controllers/Category.ts
@@ -1,20 +1,24 @@
 import CategoryModel from '../models/Category'
+import express from 'express'
 
-export const getCategories = async (req, res) => {
+export const getCategories = async (req: express.Request, res: express.Response) => {
   const categories = await CategoryModel.find({});
+
   res.status(200).send({
     success: true,
     data: categories
   })
 }
 
-export const getCategoryById = async (req, res) => {
-  const { categoryId } = req.params
-  const category = await CategoryModel.find({ id: categoryId });
+export const getCategoryById = async (req: express.Request, res: express.Response) => {
+  const { id } = req.params
+  const category = await CategoryModel.findOne({ id });
+
   if (!category) return res.status(500).send({
     success: false,
     message: 'Category not found'
   })
+
   res.status(200).send({
     success: true,
     data: category
