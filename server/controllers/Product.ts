2d050e90bb0815e43700b02ea2bc40a3d2fa681e
Son Trinh <sontrinh179@gmail.com>
2021-02-02 00:39:33 +0700

    [BE] Fix type for getProductById and add total for getProducts controllers

--- server/controllers/Product.ts
+++ server/controllers/Product.ts
@@ -1,26 +1,28 @@
 import ProductModel from '../models/Product'
-
-export const getProducts = async (req, res) => {
+import express from 'express'
+export const getProducts = async (req: express.Request, res: express.Response) => {
   const { categoryId, pageNumber = 0, pageSize = 8 } = req.query;
 
   const products = await ProductModel.find({ categoryId })
     .sort({ _id: -1 })
-    .skip((parseInt(pageNumber.toString()) - 1) * parseInt(pageSize.toString()))
+    .skip((parseInt(pageNumber.toString())) * parseInt(pageSize.toString()))
     .limit(parseInt(pageSize.toString()))
+  const total = await ProductModel.countDocuments({})
   return res.status(200).send({
     success: true,
-    data: products
+    data: products,
+    total
   })
 }
 
-export const getProductById = async (req, res) => {
+export const getProductById = async (req: express.Request, res: express.Response) => {
   const { productId } = req.params;
   const product = await ProductModel.find({ itemId: productId })
   if (!product) return res.status(500).send({
     success: false,
     message: 'Product not found'
   })
-  return res.status(200).status({
+  return res.status(200).send({
     success: true,
     data: product
   })
