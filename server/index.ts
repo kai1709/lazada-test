2ca70df1f8742e158bb51ffea03255c714890d8a
Son Trinh <sontrinh179@gmail.com>
2021-02-02 00:40:08 +0700

    [BE] Add cors config and routes for category

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import config from './config'
 import { connectDatabase } from './database'
-import ProductModel from './models/Product'
-import CategoryModel from './models/Category'
+import * as CategoryControllers from './controllers/Category'
+import * as ProductControllers from './controllers/Product'
 import bodyParser from 'body-parser'
+import cors from 'cors'
+
 const app = express();
 
 connectDatabase();
@@ -13,7 +15,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 
+app.use(cors())
+
 app.get('/', (req, res) => res.send('API is running'));
+app.get('/category/list', CategoryControllers.getCategories)
+app.get('/category/:id', CategoryControllers.getCategoryById)
+app.get('/product/list', ProductControllers.getProducts)
 
 app.listen(config.PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${config.PORT}`);
